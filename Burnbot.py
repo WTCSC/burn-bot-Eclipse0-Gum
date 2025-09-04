06a2900a7b8b322d4c98258b8e7843f9347d8599
Eclipse0-Gum <mendozac@CSGP.EDU>
2025-09-04 09:07:14 -0600

    Burnbot.py

---
+++ Burnbot.py
@@ -0,0 +1,13 @@
+name=input("What is your name?")
+hobby=input("Got a hobby you enjoy?")
+car=input("What kind of car you own?")
+school=input("What about education, where do you go?")
+face=input("Think you got a nice face?")
+
+print("\n -----Burn Bot Activated----- \n")
+
+print(f"So your name is {name}? Sounds like a name a two year old would make")
+print(f"You enjoy {hobby}? No wonder you're a nerd")
+print(f"You own a {car}? I bet you drive like your great great granny")
+print(f"You go to {school}? I can see why you look so boring")
+print(f"You said {face}? Guess what? You look uglier than a sack of potatoes")
